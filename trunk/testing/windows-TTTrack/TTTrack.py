d8adca4ba0b4b6b634434d891130237226783e65
fergalogrady <fergalogrady@ab5cc7db-bffd-c57c-7c0e-d0a6c631463b>
2013-01-20 23:13:35 +0000

    Began code for Arduino (simulates data and communicates with TTTrack.py) Added SerialMonitorThread to TTTrack.py to communicate with arduino_code

--- trunk/testing/windows-TTTrack/TTTrack.py
+++ trunk/testing/windows-TTTrack/TTTrack.py
@@ -1,24 +1,27 @@
 import pyttsx
 import serial
+import struct
+import thread
+import Queue
+from time import sleep
 from Tkinter import *
 
-
-#usage of text to speech engine:
-#engine = pyttsx.init()
-#engine.say('45.7 kilometres per hour')
-#engine.say('60 kilometres per hour.')
-#engine.runAndWait()
-
-#scan() returns the [(number,portstr), ...] of each available port
 
 #Tk tutorials
 #http://www.tkdocs.com/tutorial/index.html
 
-#need to asyn monitor serial port 
-#http://stackoverflow.com/questions/459083/how-do-you-run-your-own-code-alongside-tkinters-event-loop
-#http://code.activestate.com/recipes/82965-threads-tkinter-and-asynchronous-io/
-#http://matteolandi.blogspot.ie/2012/06/threading-with-tkinter-done-properly.html
+serial_data = Queue.Queue(1)
 
+#Serial Port Communication Thread
+#######################################
+
+def SerialMonitorThread(port):
+    ser = serial.Serial(port,9600)
+    while(1):
+        if ser.read() == 'H':
+            answer = struct.unpack('I', ser.read(4))[0]
+        if ser.read() == 'E':
+            serial_data.put(answer)
 
 #Serial Port Utility Functions
 #########################################
@@ -36,24 +39,35 @@ def scan():
    return available
  
 def generatePortsTuple():
+    #returns ('COMX','COMY', ... or ('No Ports',)
     ports = tuple([port for num, port in scan()]) 
     return ports if len(ports) > 0 else tuple(["No Ports"])
 
-##########################################
-
 
 class App:
-
+    
     def __init__(self, master):
 
-        frame = Frame(master)
-        frame.pack()
+        self.frame = Frame(master)
+        self.frame.pack()
+        
+        self.sbartext = StringVar(self.frame)
+        self.current_port = StringVar(self.frame)
+        self.speech_enabled = IntVar(value=1)
         
         #Setup serial ports / options
-        self.current_port = StringVar(frame)
+        
         self.current_port.set(generatePortsTuple()[0])
         
-        self.serialports = OptionMenu(frame, 
+        if self.current_port.get() == "No Ports":
+          self.sbartext.set("No Sensor")
+        else:
+          thread.start_new_thread(SerialMonitorThread, (self.current_port.get(),))
+          self.sbartext.set("Awaiting reading")
+          
+        # Set up serial port option menu
+        
+        self.serialports = OptionMenu(self.frame, 
                                       self.current_port, 
                                       *generatePortsTuple(), 
                                       command=self.updateSerialPort
@@ -61,24 +75,23 @@ class App:
         self.serialports.pack(side=LEFT)
 
         #Text to speech on/off button
-        self.speech_enabled = IntVar()
-        self.speechOnOff = Checkbutton(frame, 
+        self.engine = pyttsx.init()
+        
+        self.speechOnOff = Checkbutton(self.frame, 
                                         text="Talk when new speed detected",
-                                        variable=self.speech_enabled, 
-                                        command=self.speechChanger
+                                        variable=self.speech_enabled
                                         )
         self.speechOnOff.pack(side=LEFT)
 
         #Exit Button
-        self.button = Button(frame, 
+        self.button = Button(self.frame, 
                               text="QUIT", 
                               fg="red", 
-                              command=frame.quit
+                              command=self.frame.quit
                               )
         self.button.pack(side=RIGHT)
 
         #Status bar setup
-        self.sbartext = StringVar(frame)
         self.sbarframe = Frame(master)
         self.sbarlabel = Label(self.sbarframe,
                                        bd = 1,
@@ -90,17 +103,23 @@ class App:
                                        )
         self.sbarframe.pack(side = BOTTOM, fill = X)
         self.sbarlabel.pack(fill = X)
+        
+        #Start reading sensor values
+        self.readSensor()
 
-        if self.current_port.get() == "No Ports":
-          self.sbartext.set("No Sensor")
-        else:
-          self.sbartext.set("Awaiting reading")
-
-    def speechChanger(self):
-        #may not be necessary
-        print self.speech_enabled.get()
+    
+    def readSensor(self):
+        if serial_data.full():
+                self.sbartext.set(str(serial_data.get()))
+                serial_data.task_done()
+                self.frame.update()
+                if self.speech_enabled.get():
+                    self.engine.say(self.sbartext.get() + " kilometres per hour")
+                    self.engine.runAndWait()
+        self.frame.after(50, self.readSensor)
     
     def updateSerialPort(self, portselection):
+        #this does nothing yet but should restart serial port thread with new port
         print portselection
         print self.current_port.get()
 
---
+++ trunk/testing/windows-TTTrack/arduino_code/arduino_code.ino
@@ -0,0 +1,29 @@
+/*
+ * Max count in result.val: 4294967295 
+ * 
+*/
+
+//This will be used to store result and send to host machine
+union bindata {
+  unsigned long val;
+  byte b[4];
+} result;
+
+void sendResult()
+{
+    Serial.write('H');
+    Serial.write(result.b,4);
+    Serial.write('E');
+}
+  
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  result.val = 0xFFFFFFFF;
+  sendResult();
+  delay (1000);
+}
