525a5441cbe84a149c191d23d57e6747e178db81
fergalogrady <fergalogrady@ab5cc7db-bffd-c57c-7c0e-d0a6c631463b>
2013-01-21 16:22:07 +0000

    Updated to include dependency dlls list

--- trunk/testing/windows-TTTrack/setup.py
+++ trunk/testing/windows-TTTrack/setup.py
@@ -18,3 +18,22 @@ setup(windows=[{'script': 'TTTrack.py'}],
         } 
     } 
 ) 
+
+"""
+Build currently depends on: 
+ OLEAUT32.dll - C:\Windows\system32\OLEAUT32.dll
+ USER32.dll - C:\Windows\system32\USER32.dll
+ IMM32.dll - C:\Windows\system32\IMM32.dll
+ SHELL32.dll - C:\Windows\system32\SHELL32.dll
+ ole32.dll - C:\Windows\system32\ole32.dll
+ COMDLG32.dll - C:\Windows\system32\COMDLG32.dll
+ COMCTL32.dll - C:\Windows\system32\COMCTL32.dll
+ ADVAPI32.dll - C:\Windows\system32\ADVAPI32.dll
+ GDI32.dll - C:\Windows\system32\GDI32.dll
+ WS2_32.dll - C:\Windows\system32\WS2_32.dll
+ WINSPOOL.DRV - C:\Windows\system32\WINSPOOL.DRV
+ mfc90.dll - C:\Python27\lib\site-packages\Pythonwin\mfc90.dll
+ VERSION.dll - C:\Windows\system32\VERSION.dll
+ KERNEL32.dll - C:\Windows\system32\KERNEL32.dll
+
+ """
